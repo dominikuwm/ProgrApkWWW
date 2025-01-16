5cfc6e7ffb66118da74301480b932561d6ebd241
dominikuwm <115537733+dominikuwm@users.noreply.github.com>
2025-01-16 09:42:50 +0100

    Delete lab12.1 directory

--- lab12.1/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Plik: logout.php
-session_start();  // Rozpoczęcie sesji
-session_unset();  // Usunięcie wszystkich zmiennych sesji
-session_destroy();  // Zniszczenie sesji
-header('Location: ../index.php');  // Przekierowanie na stronę główną
-exit;
-?>
